cebba42795584cead9e5f21c6db13e1f393a0534
a-bentofreire <business@a-bentofreire.com>
2024-08-21 21:12:17 -0300

    Update to v1.0.4

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Description
 
-[RenameToIX](https://www.devtoix.com/en/projects/renametoix) is a Gtk3 file renamer designed to be an alternative to [Linux Mint](https://www.linuxmint.com/) bulky as bulk file renamer on nemo File Manager.
+[RenameToIX](https://www.devtoix.com/en/projects/renametoix) is a Gtk3 File Renamer designed to be an alternative to [Linux Mint](https://www.linuxmint.com/) bulky as file renamer on Nemo and Thunar File Manager.
 
 ![Image](https://www.devtoix.com/files/projects/renametoix/example-macro.gif)
 
 ## Features
 
+- GUI and Console mode.
 - Counter, file datetime, and extension Macros.
 - Start index for counter Macro.
 - Configurable list of macros.
@@ -36,11 +37,48 @@ RenameToIX uses `xdg-open` and `notify-send` external commands.
 - `%M` - file 2-digit minute
 - `%S` - file 2-digit second
 
-## Configure nemo
+## Running in console mode
+
+To activate on console mode, use `--console` on command line:
+
+```
+usage: renametoix [-h] [-console] [-start-index START_INDEX] [-reg-ex] [-include-ext] [-find FIND] [-replace REPLACE] [-allow-revert] [-test-mode] [-revert-last] [files ...]
+
+positional arguments:
+  files                 Source files
+
+options:
+  -h, --help            show this help message and exit
+  -console              Console mode
+  -start-index START_INDEX
+                        Start index used with there is a %0n macro
+  -reg-ex               Uses regular expressions on the find field
+  -include-ext          Renames including the file extension
+  -find FIND            Text to Find
+  -replace REPLACE      Text to Replace
+  -allow-revert         Generates a revert file (console mode)
+  -test-mode            Outputs only the new result, doesn't rename (console mode)
+  -revert-last          Reverts last rename and exits
+```
+
+## Revert the last rename in console mode
+
+If the previous console mode rename was executed with `-allow-revert`, then:  
+`renametoix -revert-last` will revert the last rename.
+
+## Configure Nemo
 
 To use `renametoix` as a file renamer instead of `bulky` on nemo tweak the following setting:
 
 Menu Edit -> Preferences -> Behavior -> Bulk Rename
+
+## Configure Thunar
+
+Menu Edit -> Configure Custom Actions -> "+" sign -> Edit Action  
+Name: RenameToIX  
+Command: /usr/bin/renametoix %F  
+
+RenameToIX will appear on the context menu.
 
 ## License
 
--- usr/lib/renametoix/renametoix.py
+++ usr/lib/renametoix/renametoix.py
@@ -11,6 +11,7 @@
 import io
 import os
 import re
+import argparse
 import setproctitle
 import stat
 import sys
@@ -21,13 +22,31 @@ import gi
 gi.require_version("Gtk", "3.0")  # noqa
 from gi.repository import Gtk,  Gdk, GLib
 
-setproctitle.setproctitle("renametoix")
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("-console", action='store_true', default=False, help="Console mode")  # noqa
+arg_parser.add_argument("-start-index", type=int, default=1, help="Start index used with there is a %%0n macro")  # noqa
+arg_parser.add_argument("-reg-ex", action='store_true', default=False, help="Uses regular expressions on the find field")  # noqa
+arg_parser.add_argument("-include-ext", action='store_true', default=False,
+                        help="Renames including the file extension")
+arg_parser.add_argument("-find", default="", help="Text to Find")
+arg_parser.add_argument("-replace", default="", help="Text to Replace")
+arg_parser.add_argument("-allow-revert", action='store_true', default=False,
+                        help="Generates a revert file (console mode)")
+arg_parser.add_argument("-test-mode", action='store_true', default=False,
+                        help="Outputs only the new result, doesn't rename (console mode)")
+arg_parser.add_argument("-revert-last", action='store_true', default=False,
+                        help="Reverts last rename and exits")
+arg_parser.add_argument("files", nargs="*", help="Source files")
+args = arg_parser.parse_args()
 
 
-class MainWindow:
+# ------------------------------------------------------------------------
+#                               ConsoleRename
+# ------------------------------------------------------------------------
 
-    def __init__(self):
-        self.ready = False
+class ConsoleRename:
+    def __init__(self, args):
+        self.args = args
         self.cfg = {
             "version": 1.0,
             "revert-path": os.path.join(os.environ["HOME"], ".revert-renames"),
@@ -39,9 +58,143 @@ class MainWindow:
         self.load_cfg()
         self.files = []
         self.renames = []
-        self.allow_renames = False
-        self.sort_column = None
         self.rename_count = 0
+        self.allow_renames = False
+        self.files_list_store = []
+
+    def load_cfg(self):
+        if os.path.isfile(self.cfg_name):
+            with io.open(self.cfg_name, "r", encoding="utf8") as input_file:
+                self.cfg = yaml.safe_load(input_file)
+            input_file.close()
+
+    def apply_macros(self, text, start_index, filename):
+        text = re.sub(r"%(\d*)n", lambda m: "%0*d" % (len(m.group(1)) + 1, start_index), text)
+        stamp_parts = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime(os.path.getmtime(filename))).split("_")
+        for index, macro_name in enumerate("YmdHMS"):
+            text = text.replace(f"%{macro_name}", stamp_parts[index])
+        text = text.replace(f"%E", os.path.splitext(filename)[1])
+        return text
+
+    def add_revert(self, fullname, new_fullname, basename, new_basename):
+        fullname = os.path.abspath(fullname)
+        new_fullname = os.path.abspath(new_fullname)
+        if self.rename_count == 0:
+            if not os.path.exists(self.cfg["revert-path"]):
+                os.makedirs(self.cfg["revert-path"], 0o700)
+            self.revert_name = os.path.join(self.cfg["revert-path"], "revert-rename-") + \
+                time.strftime("%Y-%m-%d-%H_%M_%S.sh", time.localtime())
+            self.revert_file = open(self.revert_name, "w")
+            self.revert_file.write("echo Reverting Changes:\n\n")
+
+        self.revert_file.write(f"printf \"'{new_basename}' → '{basename}'" +
+                               f"\\n\"\nmv '{new_fullname}' '{fullname}'\n")
+
+    def generate_new_names(self, start_index, is_reg_ex, include_ext, before, after):
+        new_filenames = set()
+        self.renames.clear()
+        self.allow_renames = True
+        for index, filename in enumerate(self.files):
+            basename = os.path.basename(filename)
+            dirname = os.path.dirname(filename)
+            from_text, ext = os.path.splitext(basename) if not include_ext else (basename, None)
+            new_text = re.sub(before, after, from_text, flags=re.A) if is_reg_ex \
+                else from_text.replace(before, after)
+            if new_text != from_text:
+                if re.search(r"%[0A-Za-z]", new_text):
+                    new_text = self.apply_macros(new_text, start_index, filename)
+                    start_index += 1
+                new_basename = (new_text + ext) if not include_ext else new_text
+                new_filename = os.path.join(dirname, new_basename)
+                self.files_list_store[index] = [dirname, basename, new_basename]
+                if new_text:
+                    self.renames.append([filename, new_filename])
+                    self.allow_renames = self.allow_renames and new_filename not in new_filenames \
+                        and not os.path.exists(new_filename)
+                    if new_filename not in new_filenames:
+                        new_filenames.add(new_filename)
+                else:
+                    self.allow_renames = False
+            else:
+                self.files_list_store[index] = [dirname, basename, basename]
+
+    def add_files(self, filenames):
+        for filename in filenames:
+            if os.path.exists(filename) and filename not in self.files:
+                basename = os.path.basename(filename)
+                self.files_list_store.append([os.path.dirname(filename), basename, basename])
+                self.files.append(filename)
+        self.update_renames()
+
+    def add_source_files(self):
+        self.add_files([uri if not uri.startswith("file://") else
+                        GLib.filename_from_uri(uri)[0] for uri in self.args.files])
+
+    def update_renames(self):
+        pass
+
+    def console_apply_renames(self, allow_revert, test_mode=False):
+        if self.allow_renames:
+            try:
+                for rename in self.renames:
+                    src_file, dst_file = rename
+                    if not os.path.exists(dst_file):
+                        if not test_mode:
+                            os.rename(src_file, dst_file)
+                            if allow_revert:
+                                self.add_revert(src_file, dst_file,
+                                                os.path.basename(src_file), os.path.basename(dst_file))
+                            self.rename_count += 1
+                        else:
+                            print(f"{src_file} -> {os.path.basename(dst_file)}")
+            finally:
+                self.close_revert_file()
+
+    def close_revert_file(self):
+        if self.rename_count and self.revert_file:
+            self.revert_file.close()
+            os.chmod(self.revert_name, stat.S_IEXEC | stat.S_IREAD | stat.S_IWRITE)
+            main_revert_name = os.path.join(self.cfg["revert-path"], "revert-rename.sh")
+            main_revert_file = open(main_revert_name, "w")
+            main_revert_file.write(f"{self.revert_name}\n")
+            main_revert_file.close()
+            os.chmod(main_revert_name, stat.S_IEXEC | stat.S_IREAD | stat.S_IWRITE)
+
+    def console_mode_rename(self):
+        if args.revert_last:
+            last_rename = os.path.join(self.cfg["revert-path"], "revert-rename.sh")
+            if os.path.exists(last_rename):
+                os.system(last_rename)
+                os.unlink(last_rename)
+                exit(1)
+            else:
+                sys.stderr.write(f"{last_rename} doesn't exists")
+                exit(0)
+
+        self.add_source_files()
+        if not self.files:
+            sys.stderr.write("No source files")
+            exit(1)
+        self.generate_new_names(self.args.start_index, self.args.reg_ex, self.args.include_ext,
+                                self.args.find, self.args.replace)
+        if not self.allow_renames:
+            sys.stderr.write("Rename conflict")
+            exit(1)
+        self.console_apply_renames(self.args.allow_revert, self.args.test_mode)
+        if self.rename_count:
+            print(f"{self.rename_count} files renamed")
+
+# ------------------------------------------------------------------------
+#                               GUIRename
+# ------------------------------------------------------------------------
+
+
+class GUIRename(ConsoleRename):
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.ready = False
+        self.sort_column = None
         self.current_folder = None
 
         self.builder = Gtk.Builder()
@@ -88,14 +241,19 @@ class MainWindow:
         self.connect("open_revert_folder_button", [[self.open_revert_folder_button_clicked]])
         self.about_dialog = self.builder.get_object("about_dialog")
 
-        self.add_files([uri if not uri.startswith("file://") else
-                        GLib.filename_from_uri(uri)[0] for uri in sys.argv[1:]])
+        self.start_index_label_spin.set_value(self.args.start_index)
+        self.reg_ex_button.set_active(self.args.reg_ex)
+        self.include_ext_button.set_active(self.args.include_ext)
+        self.find_entry.set_text(self.args.find)
+        self.replace_entry.set_text(self.args.replace)
 
+        self.add_source_files()
         screen = Gdk.Display.get_default().get_monitor(0).get_geometry()
         self.app_window.set_default_size(screen.width, screen.height - 100)
         self.app_window.connect("delete-event", Gtk.main_quit)
         self.app_window.show_all()
         self.ready = True
+        self.update_renames()
 
     # Events
 
@@ -189,60 +347,10 @@ class MainWindow:
 
     # Methods
 
-    def add_files(self, filenames):
-        for filename in filenames:
-            if os.path.exists(filename) and filename not in self.files:
-                basename = os.path.basename(filename)
-                self.files_list_store.append([os.path.dirname(filename), basename, basename])
-                self.files.append(filename)
-        self.update_renames()
-
-    def add_revert(self, fullname, new_fullname, basename, new_basename):
-        fullname = os.path.abspath(fullname)
-        new_fullname = os.path.abspath(new_fullname)
-        if self.rename_count == 0:
-            if not os.path.exists(self.cfg["revert-path"]):
-                os.makedirs(self.cfg["revert-path"], 0o700)
-            self.revert_name = os.path.join(self.cfg["revert-path"], "revert-rename-") + \
-                time.strftime("%Y-%m-%d-%H_%M_%S.sh", time.localtime())
-            self.revert_file = open(self.revert_name, "w")
-            self.revert_file.write("echo Reverting Changes:\n\n")
-
-        self.revert_file.write(f"printf \"'{new_basename}' → '{basename}'" +
-                               f"\\n\"\nmv '{new_fullname}' '{fullname}'\n")
-
-    def apply_macros(self, text, start_index, filename):
-        text = re.sub(r"%(\d*)n", lambda m: "%0*d" % (len(m.group(1)) + 1, start_index), text)
-        stamp_parts = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime(os.path.getmtime(filename))).split("_")
-        for index, macro_name in enumerate("YmdHMS"):
-            text = text.replace(f"%{macro_name}", stamp_parts[index])
-        text = text.replace(f"%E", os.path.splitext(filename)[1])
-        return text
-
     def apply_renames(self):
         if self.allow_renames:
-            try:
-                for rename in self.renames:
-                    src_file, dst_file = rename
-                    if not os.path.exists(dst_file):
-                        os.rename(src_file, dst_file)
-                        if self.cfg["allow-revert"]:
-                            self.add_revert(src_file, dst_file, os.path.basename(src_file), os.path.basename(dst_file))
-                        self.rename_count += 1
-            finally:
-                self.close_revert_file()
-
+            self.console_apply_renames(self.cfg["allow-revert"])
             self.notify_msg(f"{self.rename_count} files renamed")
-
-    def close_revert_file(self):
-        if self.rename_count:
-            self.revert_file.close()
-            os.chmod(self.revert_name, stat.S_IEXEC | stat.S_IREAD | stat.S_IWRITE)
-            main_revert_name = os.path.join(self.cfg["revert-path"], "revert-rename.sh")
-            main_revert_file = open(main_revert_name, "w")
-            main_revert_file.write(f"{self.revert_name}\n")
-            main_revert_file.close()
-            os.chmod(main_revert_name, stat.S_IEXEC | stat.S_IREAD | stat.S_IWRITE)
 
     def close_window(self, widget=None):
         Gtk.main_quit()
@@ -260,12 +368,6 @@ class MainWindow:
         for event in events:
             object.connect(event[1] if len(event) == 2 else "clicked", event[0])
         return object
-
-    def load_cfg(self):
-        if os.path.isfile(self.cfg_name):
-            with io.open(self.cfg_name, "r", encoding="utf8") as input_file:
-                self.cfg = yaml.safe_load(input_file)
-            input_file.close()
 
     def notify_msg(self, msg=""):
         if self.cfg["send-notification"]:
@@ -287,43 +389,15 @@ class MainWindow:
             macros_popup.append(menuitem)
 
     def update_renames(self, widget=None):
-        if not self.ready:
-            return
-
-        start_index = self.start_index_label_spin.get_value_as_int()
-        is_reg_ex = self.reg_ex_button.get_active()
-        include_ext = self.include_ext_button.get_active()
-        before = self.find_entry.get_text()
-        after = self.replace_entry.get_text()
-
-        new_filenames = set()
-        self.renames.clear()
-        self.allow_renames = True
-        for index, filename in enumerate(self.files):
-            basename = os.path.basename(filename)
-            dirname = os.path.dirname(filename)
-            from_text, ext = os.path.splitext(basename) if not include_ext else (basename, None)
-            new_text = re.sub(before, after, from_text, flags=re.A) if is_reg_ex \
-                else from_text.replace(before, after)
-            if new_text != from_text:
-                if re.search(r"%[0A-Za-z]", new_text):
-                    new_text = self.apply_macros(new_text, start_index, filename)
-                    start_index += 1
-                new_basename = (new_text + ext) if not include_ext else new_text
-                new_filename = os.path.join(dirname, new_basename)
-                self.files_list_store[index] = [dirname, basename, new_basename]
-                if new_text:
-                    self.renames.append([filename, new_filename])
-                    self.allow_renames = self.allow_renames and new_filename not in new_filenames \
-                        and not os.path.exists(new_filename)
-                    if new_filename not in new_filenames:
-                        new_filenames.add(new_filename)
-                else:
-                    self.allow_renames = False
-            else:
-                self.files_list_store[index] = [dirname, basename, basename]
-
-        self.ok_button.set_sensitive(self.allow_renames)
+        if self.ready:
+            self.generate_new_names(
+                self.start_index_label_spin.get_value_as_int(),
+                self.reg_ex_button.get_active(),
+                self.include_ext_button.get_active(),
+                self.find_entry.get_text(),
+                self.replace_entry.get_text()
+            )
+            self.ok_button.set_sensitive(self.allow_renames)
 
     def save_cfg(self):
         cfg_path = os.path.dirname(self.cfg_name)
@@ -336,5 +410,9 @@ class MainWindow:
             self.update_macro_widgets()
 
 
-win = MainWindow()
-Gtk.main()
+if not args.console and not args.revert_last:
+    setproctitle.setproctitle("renametoix")
+    win = GUIRename(args)
+    Gtk.main()
+else:
+    ConsoleRename(args).console_mode_rename()
